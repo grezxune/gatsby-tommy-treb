700ef3387ba8ff7c26388d0f5614e045b7930950
Tommy Treb <tomtrezb2003@gmail.com>
2020-01-19 08:56:48 -0600

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
         <p>
           These are the words I would use to describe my life if I only had 4.28
           seconds to do it. I had more time than that while making this
-          document, but I think they do a pretty good job.
+          document, but I think they do a pretty good job as is.
         </p>
         <p>
           So you're here to get your eyes on some content. Start by observing me
@@ -39,13 +39,13 @@ const IndexPage = () => {
         </p>
         <ProfileImage src={profileImage} alt="profile image" />
         <p>
-          Now that we've got that out of the way, you can get on to the probably
-          more interesting stuff, so I invite you to open your mind and start by
-          checking out my <Link to="/blog">blog</Link> or{" "}
+          Now that we've got that out of the way, you can get on to the boring
+          stuff. I invite you to open your mind and start by checking out my{" "}
+          <Link to="/blog">blog</Link> or my{" "}
           <Link to="/projectList">projects</Link>. You will get to know more
-          about myself (if that's not something you're interested in, turn back
-          now!), and may pick up a couple of things you'll find useful in your
-          life.
+          about me. If that's not something you're interested in, turn back now!
+          If you decide to delve into this content, you may even pick up a
+          couple of things you'll find useful in your life!
         </p>
       </Container>
     </Layout>
